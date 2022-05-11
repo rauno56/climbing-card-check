c2982385475f0c4c3f0dae016cd5069ecac77bde
Rauno Viskus <rauno56@gmail.com>
2022-05-11 19:33:18 +0300

    feat: build site

---
+++ public/app.js
@@ -0,0 +1,21 @@
+ async function fetchResult(id) {
+	const res = await fetch(`/api/check/${id}`);
+	const data = await res.text();
+	return data;
+}
+
+async function logSubmit(event) {
+	event.preventDefault();
+
+	log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+	console.log(event);
+	const result = await fetchResult(idCodeInput.value);
+
+	log.textContent += result;
+}
+
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+const idCodeInput = document.getElementById('idCode');
+
+form.addEventListener('submit', logSubmit);
