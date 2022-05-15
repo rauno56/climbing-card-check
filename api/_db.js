ab857b10358502255cf002ec9fe600c002a93ba0
Rauno Viskus <rauno56@gmail.com>
2022-05-15 22:48:23 +0300

    style: unify indentation

--- api/_db.js
+++ api/_db.js
@@ -10,10 +10,10 @@ const sheets = google.sheets('v4');
 assert.equal(typeof spreadsheetId, 'string', 'Expected SPREADSHEET_ID env var to be set');
 
 const CODE = {
-  GREEN: 'green',
-  RED: 'red',
-  INSTRUCTOR: 'instructor',
-  UNKNOWN: 'unknown',
+	GREEN: 'green',
+	RED: 'red',
+	INSTRUCTOR: 'instructor',
+	UNKNOWN: 'unknown',
 };
 
 const RAW_VALUE_TO_CODE = {
@@ -23,37 +23,37 @@ const RAW_VALUE_TO_CODE = {
 };
 
 const testCodes = [
-  '1',
-  '2',
-  '3',
+	'1',
+	'2',
+	'3',
 ];
 
 const assertValidId = (id) => {
-  assert.equal(typeof id, 'string', 'Expected ID to be a string');
-  assert.match(id, /[0-9]{11}/, 'Expected ID to consist of 11 digits');
+	assert.equal(typeof id, 'string', 'Expected ID to be a string');
+	assert.match(id, /[0-9]{11}/, 'Expected ID to consist of 11 digits');
 };
 
 const connect = (serviceAccountEmail, privateKey) => {
 	assert.equal(typeof serviceAccountEmail, 'string', '"serviceAccountEmail" required');
 	assert.equal(typeof privateKey, 'string', '"serviceAccountEmail" required');
 
-  const jwtClient = new google.auth.JWT(
-    serviceAccountEmail,
-    null,
-    privateKey,
-    ['https://www.googleapis.com/auth/spreadsheets']
-  );
-  return jwtClient.authorize().then(() => jwtClient);
+	const jwtClient = new google.auth.JWT(
+		serviceAccountEmail,
+		null,
+		privateKey,
+		['https://www.googleapis.com/auth/spreadsheets']
+	);
+	return jwtClient.authorize().then(() => jwtClient);
 }
 
 const fetchAllData = async (client)  => {
 	assert(client instanceof google.auth.JWT, '"client" required');
 
-  return sheets.spreadsheets.values.get({
-     auth: client,
-     spreadsheetId: spreadsheetId,
-     range: sheetRange
-  });
+	return sheets.spreadsheets.values.get({
+		 auth: client,
+		 spreadsheetId: spreadsheetId,
+		 range: sheetRange
+	});
 };
 
 // raw input from the sheet => valueof CODE
@@ -78,52 +78,52 @@ const nameHeader = 'Nimi';
 const fetchOne = async (client, id) => {
 	assert(client instanceof google.auth.JWT, `"client" required got ${inspect(client)}`);
 
-  if (!testCodes.includes(id)) {
-    assertValidId(id);
-  }
+	if (!testCodes.includes(id)) {
+		assertValidId(id);
+	}
 
-  const { data: { values: data } } = await fetchAllData(client);
+	const { data: { values: data } } = await fetchAllData(client);
 
-  const headers = data.shift();
+	const headers = data.shift();
 
-  const filterColumnIdx = headers.indexOf(filterColumnHeader);
-  const certificateColumnIdx = headers.indexOf(certificateHeader);
-  const nameColumnIdx = headers.indexOf(nameHeader);
+	const filterColumnIdx = headers.indexOf(filterColumnHeader);
+	const certificateColumnIdx = headers.indexOf(certificateHeader);
+	const nameColumnIdx = headers.indexOf(nameHeader);
 
-  assert(~filterColumnIdx, `Filter column not found. Looked for ${filterColumnHeader}`);
-  assert(~certificateColumnIdx, `Certificate column not found. Looked for ${certificateHeader}`);
-  assert(~nameColumnIdx, `Certificate column not found. Looked for ${nameHeader}`);
+	assert(~filterColumnIdx, `Filter column not found. Looked for ${filterColumnHeader}`);
+	assert(~certificateColumnIdx, `Certificate column not found. Looked for ${certificateHeader}`);
+	assert(~nameColumnIdx, `Certificate column not found. Looked for ${nameHeader}`);
 
-  const filteredRows = data.filter((row) => {
-  	return row[filterColumnIdx] === id;
-  });
+	const filteredRows = data.filter((row) => {
+		return row[filterColumnIdx] === id;
+	});
 
-  // no result
-  if (!filteredRows.length) {
-  	return {
-  		id,
-  		success: false,
-  	};
-  }
+	// no result
+	if (!filteredRows.length) {
+		return {
+			id,
+			success: false,
+		};
+	}
 
-  // more than one result, assume an filtering error
-  if (filteredRows.length > 1) {
-  	console.log('Filtered more than one row', filteredRows);
-  	return {
-  		id,
-  		success: false,
-  	};
-  }
+	// more than one result, assume an filtering error
+	if (filteredRows.length > 1) {
+		console.log('Filtered more than one row', filteredRows);
+		return {
+			id,
+			success: false,
+		};
+	}
 
-  const row = filteredRows[0];
-  const certificate = normalizeCertificate(row[certificateColumnIdx]);
+	const row = filteredRows[0];
+	const certificate = normalizeCertificate(row[certificateColumnIdx]);
 
-  return {
-  	id,
-  	success: true,
-  	certificate,
-  	name: row[nameColumnIdx],
-  };
+	return {
+		id,
+		success: true,
+		certificate,
+		name: row[nameColumnIdx],
+	};
 };
 
 export { connect, fetchAllData, fetchOne };
