8ace4433e33683eb07fc38728bf762edfeb95afc
Lauri Miller <lauri.mllr@gmail.com>
2022-06-11 14:17:20 +0300

    Improve check response flow

--- public/index.js
+++ public/index.js
@@ -10,8 +10,15 @@ Vue.createApp({
     methods: {
         fetchResult: function(id) {
             fetch(`/api/check?id=${id}`)
-                .then((response) => response.json())
+                .then((response) => {
+                    if (!response.ok) {
+                        this.currentClimber = null;
+                        return;
+                    }
+                    return response.json()
+                })
                 .then((data) => {
+                    if (!data) return;
                     this.showInstructions = false;
                     this.currentClimber = data.success ? data : null;
                 });
