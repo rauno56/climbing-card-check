6bbdab8eab81117489c7a25b5f1e9026a0e6da99
Lauri Miller <lauri.mllr@gmail.com>
2022-06-20 22:44:13 +0300

    Update translations and gitignore

--- public/index.js
+++ public/index.js
@@ -1,37 +1,37 @@
 Vue.createApp({
-    data: () => ({
-        showInstructions: true,
-        currentClimber: null,
-        idCode: '',
-        isLoading: false,
-    }),
-    created() {
+  data: () => ({
+    showInstructions: true,
+    currentClimber: null,
+    idCode: '',
+    isLoading: false,
+  }),
+  created() {
 
-    },
-    methods: {
-        fetchResult: function(id) {
-            fetch(`/api/check?id=${id}`)
-                .then((response) => {
-                    if (!response.ok) {
-                        this.currentClimber = null;
-                        return;
-                    }
-                    return response.json()
-                })
-                .then((data) => {
-                    if (!data) return;
-                    this.showInstructions = false;
-                    this.currentClimber = data.success ? data : null;
-                    this.isLoading = false;
-                });
-        },
-        submit: function(event) {
-            if (!this.idCode) return;
-            this.isLoading = true;
-            const result = this.fetchResult(this.idCode);
-        },
-        goBack: function() {
+  },
+  methods: {
+    fetchResult: function(id) {
+      fetch(`/api/check?id=${id}`)
+        .then((response) => {
+          if (!response.ok) {
             this.currentClimber = null;
-        }
+            return;
+          }
+          return response.json()
+        })
+        .then((data) => {
+          if (!data) return;
+          this.showInstructions = false;
+          this.currentClimber = data.success ? data : null;
+          this.isLoading = false;
+        });
+    },
+    submit: function(event) {
+      if (!this.idCode) return;
+      this.isLoading = true;
+      const result = this.fetchResult(this.idCode);
+    },
+    goBack: function() {
+      this.currentClimber = null;
     }
-}).mount('#app')
+  }
+}).mount('#app');
