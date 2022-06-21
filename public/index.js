cdc1c29d768893853c7f726a2e10964f1cc1a609
Rauno Viskus <rauno56@gmail.com>
2022-06-21 13:12:23 +0300

    refactor: solve unused vars by removing `event` and using `result`

--- public/index.js
+++ public/index.js
@@ -10,25 +10,25 @@ Vue.createApp({
 	},
 	methods: {
 		fetchResult: function(id) {
-			fetch(`/api/check?id=${id}`)
+			return fetch(`/api/check?id=${id}`)
 				.then((response) => {
 					if (!response.ok) {
 						this.currentClimber = null;
 						return;
 					}
 					return response.json();
-				})
+				});
+		},
+		submit: function() {
+			if (!this.idCode) return;
+			this.isLoading = true;
+			this.fetchResult(this.idCode)
 				.then((data) => {
 					if (!data) return;
 					this.showInstructions = false;
 					this.currentClimber = data.success ? data : null;
 					this.isLoading = false;
 				});
-		},
-		submit: function(event) {
-			if (!this.idCode) return;
-			this.isLoading = true;
-			const result = this.fetchResult(this.idCode);
 		},
 		goBack: function() {
 			this.currentClimber = null;
