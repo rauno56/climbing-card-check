a6f205aea3a45dfd3ab8cfefd1679fe03300c491
Rauno Viskus <Rauno56@users.noreply.github.com>
2022-06-22 13:57:35 +0300

    chore: add `Vue` to globals and remove unused vars (#8)
    * chore: add `Vue` to globals
    
    * refactor: solve unused vars by removing `event` and using `result`
    
    * style: add spaces after function keywords to make them look less like function calls

--- public/index.js
+++ public/index.js
@@ -9,15 +9,20 @@ Vue.createApp({
 
 	},
 	methods: {
-		fetchResult: function(id) {
-			fetch(`/api/check?id=${id}`)
+		fetchResult: function (id) {
+			return fetch(`/api/check?id=${id}`)
 				.then((response) => {
 					if (!response.ok) {
 						this.currentClimber = null;
 						return;
 					}
 					return response.json();
-				})
+				});
+		},
+		submit: function () {
+			if (!this.idCode) return;
+			this.isLoading = true;
+			this.fetchResult(this.idCode)
 				.then((data) => {
 					if (!data) return;
 					this.showInstructions = false;
@@ -25,12 +30,7 @@ Vue.createApp({
 					this.isLoading = false;
 				});
 		},
-		submit: function(event) {
-			if (!this.idCode) return;
-			this.isLoading = true;
-			const result = this.fetchResult(this.idCode);
-		},
-		goBack: function() {
+		goBack: function () {
 			this.currentClimber = null;
 		}
 	}
