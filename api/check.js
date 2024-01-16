563c4dd7aeffe262581df53fd55c36ec6a5350c6
Rauno Viskus <1334536+rauno56@users.noreply.github.com>
2024-01-16 09:17:45 +0200

    feat: allow temporary records to be added via Forms (#24)
    * feat: allow adding records through Forms
    
    * feat: log request times
    
    * fix: show response info instead of the form one in result page
    
    * fix: fix expiry typo

--- api/check.js
+++ api/check.js
@@ -2,6 +2,7 @@ import * as db from './_db.js';
 import * as key from './_key.js';
 
 export default async function handler(request, response) {
+	const start = Date.now();
 	const secretKey = key.load();
 	const sheetsClient = await db.connect(secretKey.client_email, secretKey.private_key);
 
@@ -9,5 +10,6 @@ export default async function handler(request, response) {
 
 	const result = await db.fetchOne(sheetsClient, id);
 
+	console.log({ ts: new Date(), responseTime: Date.now() - start, id });
 	return response.status(200).json(result);
 }
