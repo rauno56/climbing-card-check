81f7e975a4ada45919ecf78bae2b1d5ee98673aa
Rauno Viskus <rauno56@gmail.com>
2025-08-19 23:39:08 +0300

    feat: check id code validity before going to the DB

--- api/check.js
+++ api/check.js
@@ -1,12 +1,22 @@
+import { isIdCodeValid } from './_db.data-utils.js';
 import * as db from './_db.js';
 import * as key from './_key.js';
 
 export default async function handler(request, response) {
 	const start = Date.now();
+	const { id } = request.query;
+
+	if (!isIdCodeValid(id)) {
+		console.log({ ts: new Date(), responseTime: Date.now() - start, id });
+		return response.status(200).json({
+			success: false,
+			id,
+			message: "Invalid id code",
+		});
+	}
+
 	const secretKey = key.load();
 	const sheetsClient = await db.connect(secretKey.client_email, secretKey.private_key);
-
-	const { id } = request.query;
 
 	const result = await db.fetchOne(sheetsClient, id);
 
